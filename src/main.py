98f4acf456e84c341764a52de70af7ee009f229e
alxkt <alexkturnbull@gmail.com>
2024-09-17 20:35:03 -0700

    Make point, line, draw line

--- src/main.py
+++ src/main.py
@@ -1,30 +1,11 @@
-
-from tkinter import Tk, BOTH, Canvas
-
-class Window():
-  def __init__(self, width, height):
-    self.root = Tk()
-    self.root.title = "My window"
-    self.canvas = Canvas(height=height, width=width)
-    self.canvas.pack()
-    self.running = False
-
-    self.root.protocol("WM_DELETE_WINDOW", self.close)
-
-  def redraw(self):
-    self.root.update_idletasks()
-    self.root.update()
-
-  def wait_for_close(self):
-    self.running = True
-    while(self.running):
-      self.redraw()
-
-  def close(self):
-    self.running = False
+from window import *
 
 def main():
   win = Window(800, 600)
+  p1 = Point(10,15)
+  p2 = Point(350, 400)
+  line = Line(p1, p2)
+  win.draw_line(line, "red")
   win.wait_for_close()
 
 main()
